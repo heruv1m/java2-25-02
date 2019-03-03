1ced77994a33aeaebc6b7ca30c312627462c6aaf
aleksandr.kazantsev <aleksandr.kazantsev@corp.mail.ru>
2019-03-03 11:01:55 +0300

    add lesson

---
+++ src/MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+
+
+    public void updateEntity(){};
+    public String getValue() {
+        return "";
+    }
+}
---
+++ src/MyException.java
@@ -0,0 +1,13 @@
+public class MyException extends RuntimeException{
+    public MyException(String message) {
+        super(message);
+    }
+
+    public MyException(Throwable cause) {
+        super(cause);
+    }
+
+    public MyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
